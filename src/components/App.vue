3f9fa1db332df9c7e0aca4690083ba2b83595dd0
蒲鸽 <my@owo.help>
2017-04-03 21:42:36 +0800

    修复打包问题

--- src/components/App.vue
+++ src/components/App.vue
@@ -21,6 +21,15 @@ import Search from './panel/Search'
 import AppTitle from './bar/AppTitle'
 import TitleBar from './bar/Title'
 import BottomBar from './bar/Bottom'
+import localforage from 'localforage'
+//引入图片资源
+const $tiangongyuanyuan = require('../assets/tiangongyuanyuan.png'),
+      $xinxifabu        = require('../assets/xinxifabu.png'),
+      $youjian          = require('../assets/youjian.png'),
+      $gongwenguanli    = require('../assets/gongwenguanli.png'),
+      $bangongxitong    = require('../assets/bangongxitong.png'),
+      $1    = require('../assets/1.png'),
+      $2    = require('../assets/2.png');
 export default {
   data () {
     return {
@@ -30,21 +39,21 @@ export default {
       userName:'',
       usbkeyidentification:'',
       appList:{
-        tiangongyuanyuan:{id:"10000", name:"天工圆圆", icon:'http://xn--9tr.com/vrv/304/tiangongyuanyuan.png',url:'#', special:"open"},
-        xinxifabu:{id:"10001", name:"信息发布", icon:'http://xn--9tr.com/vrv/304/xinxifabu.png',url:'http://info.casic.cs/jeecms2/index/mobile/', special:"url"},
-        youjian:{id:"10002", name:"邮件", icon:'http://xn--9tr.com/vrv/304/youjian.png',url:'http://10.152.36.31/secmail/loginapp.do?type=cid&PID='+this.usbkeyidentification, special:"url"},
-        gongwenguanli:{id:"10003", name:"公文管理", icon:'http://xn--9tr.com/vrv/304/gongwenguanli.png',url:'#', special:"bggl"},
-        bangongxitong:{id:"10004", name:"协同办公", icon:'http://xn--9tr.com/vrv/304/bangongxitong.png',url:'http://10.152.36.26:8080/portal/menu.jsp?userName='+this.userName+'&PID='+this.usbkeyidentification+'&webService=&SessionID=', special:"url"}
+        tiangongyuanyuan:{id:"10000", name:"天工圆圆", icon:$tiangongyuanyuan,url:'#', special:"open"},
+        xinxifabu:{id:"10001", name:"信息发布", icon:$xinxifabu,url:'http://info.casic.cs/jeecms2/index/mobile/', special:"url"},
+        youjian:{id:"10002", name:"邮件", icon:$youjian,url:'http://10.152.36.31/secmail/loginapp.do?type=cid&PID='+this.usbkeyidentification, special:"url"},
+        gongwenguanli:{id:"10003", name:"公文管理", icon:$gongwenguanli,url:'#', special:"bggl"},
+        bangongxitong:{id:"10004", name:"协同办公", icon:$bangongxitong,url:'http://10.152.36.26:8080/portal/menu.jsp?userName='+this.userName+'&PID='+this.usbkeyidentification+'&webService=&SessionID=', special:"url"}
       },
       showList:[
       {
         url: 'https://translate.google.cn/',
-        img: 'http://xn--9tr.com/vrv/304/2.png',
+        img: $1,
         title: ''
       },
       {
         url: 'http://10.152.36.31/secmail/loginapp.do?type=cid&PID='+this.usbkeyidentification,
-        img: 'http://xn--9tr.com/vrv/304/1.png',
+        img: $2,
         title: ''
       }],
     }
