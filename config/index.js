4e1e675586644912446231859c7d9a7643282d95
蒲鸽 <my@owo.help>
2017-06-03 17:07:24 +0800

    去掉自动打开浏览器配置 因为在浏览器里运行不起来

--- config/index.js
+++ config/index.js
@@ -24,7 +24,6 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
-    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
