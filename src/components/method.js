4ed19ff03df00d7556ee5799765c9ba5d832a02b
PUGE <100284685@qq.com>
2017-06-11 20:59:08 +0800

    程序内存被清恢复

--- src/components/method.js
+++ src/components/method.js
@@ -64,6 +64,7 @@ const timeoutDetection = function(){
 
 //程序内存被清恢复
 const dataDetection = function(){
+  if(Timestamp!== null) return
   //从本读数据库中取出数据
   localforage.getItem("appData",(err,appData) => {
     DATA.org = appData.org
