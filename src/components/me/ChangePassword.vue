3536816bd276a93430e3b1fe97a916adbe30e1bf
PUGE <100284685@qq.com>
2017-06-22 00:26:46 +0800

    排查改密码卡住原因

--- src/components/me/ChangePassword.vue
+++ src/components/me/ChangePassword.vue
@@ -17,7 +17,7 @@
 import TitleBar from '../brick/Title'
 import { Order } from '../Order.js'
 import Toast from '../brick/Toast'
-import { DATA, CHANNEL } from "../method.js" 
+import { DATA, CHANNEL, log } from "../method.js" 
 export default {
   components: {
     TitleBar,
@@ -30,16 +30,19 @@ export default {
       if(!this.oldPassword || !this.password || !this.repeatPassword) {Order.$emit('Toast', '当前密码不正确'); return null;} //密码验证
       if(this.password !== this.repeatPassword) {Order.$emit('Toast', '密码不一致'); return null;} //密码验证
       //更改密码信号监听
-      Order.$on('changedPwd', function(message) {
+      Order.$once('changedPwd', function(message) {
+        log(message)
         switch(message.code){
           case 543 :  Order.$emit('Toast', '当前密码不正确') ; break;
           case 542 :  Order.$emit('Toast', '新密码不合法') ; break;
           case 541 :  Order.$emit('Toast', '用户不存在') ; break;
+          case 1304:  Order.$emit('Toast', '修改失败') ; break;
           case 0   :  Order.$emit('Toast', '修改成功') ;  break;
           default  :  Order.$emit('Toast', `遇到错误:${Data.code}`) ;
         }
       })
       const data = {oldPwd : _this.oldPassword, newPwd : _this.password}
+      log("111")
       CHANNEL.changedPwd(JSON.stringify(data))
     }
   },
